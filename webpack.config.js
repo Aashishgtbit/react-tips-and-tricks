6654a330d9602f288e7fb6d0fd79f37ba804d0a6
Aashishgtbit <aashishtiwari97@gmail.com>
2020-01-22 19:29:06 +0530

    added css minify plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,13 @@ const path = require("path");
 
 // const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const PurgecssPlugin = require("purgecss-webpack-plugin");
+const glob = require("glob-all");
 
+const PATHS = {
+    src: path.join(__dirname, "src")
+};
 module.exports = {
     entry: "./src/index.js",
     output: {
@@ -10,26 +16,45 @@ module.exports = {
         chunkFilename: "[name].bundle.js",
         path: path.resolve(__dirname, "./dist")
     },
+
+    // optimization: {
+    //     splitChunks: {
+    //         cacheGroups: {
+    //             styles: {
+    //                 name: "style",
+    //                 test: /\.(s*)css$/,
+    //                 chunks: "all",
+    //                 enforce: true
+    //             }
+    //         }
+    //     }
+    // },
     plugins: [
         // new BundleAnalyzerPlugin(),
         new HtmlWebpackPlugin({
             title: "Products",
             template: "public/index.html",
             filename: "index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].scss"
+        }),
+        new PurgecssPlugin({
+            paths: glob.sync(`${PATHS.src}/**/*.scss`, { nodir: true })
         })
     ],
 
     module: {
         rules: [
             {
-                test: /\.css$/,
+                test: /\css$/,
                 exclude: /node_modules/,
-                use: ["style-loader", "css-loader"]
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
             },
             {
                 test: /\.scss$/,
                 exclude: /node_modules/,
-                use: ["style-loader", "css-loader", "sass-loader"]
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
             },
             {
                 test: /\.js$/,
